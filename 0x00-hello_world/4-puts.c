a49d9015f674583f7943651fc64f878cb3775a35
Ashebir Adugna <Diakonrobel@gmail.com>
2022-06-30 17:17:04 +0300

    Project_c Hello-world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - prints the phrase "programming is like building a multilangual puzzle"
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+	puts("\programming is like a building a multilingual puzzle");
+	return (0);
+}
