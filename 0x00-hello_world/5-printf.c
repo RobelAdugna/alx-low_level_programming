544f028193c32e2d986dad19d8c7028b68f7de1e
Ashebir Adugna <Diakonrobel@gmail.com>
2022-07-01 20:00:29 +0300

    heec

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,9 @@
 #include <stdio.h>
 
-/**
- * main prints the phrase "programming is like building a multilingual puzzle
- * but the outcome is a piece of art 
- * Return: 0 if exited properly, non-zero otherwise
- */
 int main(void)
 {
- printf("with proper grammar, but the outcome is a piece of art,\n");
- return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
 ~                                                                                                                              
 ~                                                                                                                              
