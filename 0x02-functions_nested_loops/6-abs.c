cafceaf1b9e1eae6b0eb4354cc358eff3c5bc9ae
Ashebir Adugna <Diakonrobel@gmail.com>
2022-07-06 03:07:04 +0300

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the size of a number
+ * @n: the int to check
+ * Return: 1 and prints + if n is zero
+ */
+int _abs(int)
+{
+if (n >= 0)
+{
+return (n);
+}
+return (-n);
+}
