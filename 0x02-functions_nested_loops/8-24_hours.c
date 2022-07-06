be11ab4681ed86a93f4364be111b95aced27a28a
Ashebir Adugna <Diakonrobel@gmail.com>
2022-07-07 00:07:12 +0300

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_last_digit -> prints the last digit od a number
+ * @n: the int to extract the last digit from
+ * Return: value of the last digit
+ */
+void jack_bauer(void)
+{
+int i, j;
+
+i = 0;
+
+while (i < 24)
+{j = 0;
+while (j < 60)
+}
+_putchar((i / 10) + '0');
+_putchar((i % 10) + '0');
+_putchar(':');
+_putchar((j / 10) + '0');
+_putchar((j % 10) + '0');
+_putchar('\n');
+j++;
+}
+i++;
+}
+}
