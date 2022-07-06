a865d427962539f3a014c398fe1f276415558e16
Ashebir Adugna <Diakonrobel@gmail.com>
2022-07-07 01:51:10 +0300

    times table hot fix

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -24,8 +24,8 @@ _putchar(',');
 _putchar(' ');
 _putchar(' ');
 _putchar(k + '0');
-else if (k >= 10)
 }
+else if (k >= 10)
 {
 _putchar(',');
 _putchar(' ');
@@ -33,6 +33,6 @@ _putchar((k / 10) + '0');
 _putchar((k % 10) + '0');
 }
 }
-putchar('\n')
+_putchar('\n')
 }
 }
