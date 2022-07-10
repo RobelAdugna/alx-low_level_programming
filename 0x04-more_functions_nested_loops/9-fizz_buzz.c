35c8b90480ebb6ca4aae2cef7da1caee857b949a
Ashebir Adugna <Diakonrobel@gmail.com>
2022-07-10 03:18:57 +0300

    9-fizz_buzz done time

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,13 +15,13 @@ int main(void)
 	{
 		if (i % 3 == 0 && i % 5 != 0)
 		{
-			printf(“ Fizz”);
+			printf(" Fizz");
 		} else if (i % 5 == 0 && i % 3 != 0)
 		{
-			printf(“ Buzz”);
+			printf(" Buzz");
 		} else if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf(“ FizzBuzz”);
+			printf(" FizzBuzz");
 		} else if (i == 1)
 		{
 			printf("%d", i);
@@ -30,7 +30,7 @@ int main(void)
 			printf(" %d", i);
 		}
 	}
-	printf(“\n”);
+	printf("\n");
 
 	return (0);
 }
